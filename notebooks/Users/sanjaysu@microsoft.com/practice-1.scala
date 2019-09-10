dad24c1aab2a0e3aaf9ab3443de34afb55cafea6
sanjaysusheelan <55078843+sanjaysusheelan@users.noreply.github.com>
2019-09-10 13:20:59 +0530

    version 0.1

--- notebooks/Users/sanjaysu@microsoft.com/practice-1.scala
+++ notebooks/Users/sanjaysu@microsoft.com/practice-1.scala
@@ -1,3 +1,29 @@
 // Databricks notebook source
 // MAGIC %md
 // MAGIC Learning ADB Professionaly
+// MAGIC This wud configure ADB Volume Mounting with DLS Gen2
+
+// COMMAND ----------
+
+val configs = Map(
+  "fs.azure.account.auth.type" -> "OAuth",
+  "fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+  "fs.azure.account.oauth2.client.id" -> "685e25f7-f53e-4e22-9410-acefa022bf38",
+  "fs.azure.account.oauth2.client.secret" -> dbutils.secrets.get(scope = "training-scope", key = "aadsecret"),
+  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
+
+
+
+// COMMAND ----------
+
+val configs = Map(
+  "fs.azure.account.auth.type" -> "OAuth",
+  "fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+  "fs.azure.account.oauth2.client.id" -> "685e25f7-f53e-4e22-9410-acefa022bf38",
+  "fs.azure.account.oauth2.client.secret" -> dbutils.secrets.get(scope = "training-scope", key = "aadsecret"),
+  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
+
+dbutils.fs.mount(
+  source = "abfss://data@aza4dls.dfs.core.windows.net/",
+  mountPoint = "/mnt/data",
+  extraConfigs = configs)
