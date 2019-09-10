de3d51ce7f2ca9e8c0226d65b31fe6effed74761
sanjaysusheelan <55078843+sanjaysusheelan@users.noreply.github.com>
2019-09-10 14:25:32 +0530

    Update: README.md notebooks

--- notebooks/practice-2.scala
+++ notebooks/practice-2.scala
@@ -3,3 +3,19 @@
 // MAGIC 
 // MAGIC # Load Data from DLS to ADB
 // MAGIC ## Processing Data
+
+// COMMAND ----------
+
+import spark.implicits._
+
+case class Customer (customerid: Int, fullname: String, address: String, credit: Int, status: Boolean, remarks: String)
+
+val customersCsvLocation = "/mnt/data/Customers/*.csv"
+val customers = 
+  spark
+    .read
+    .option("inferSchema", true)
+    .option("header", true)
+    .option("sep", ",")
+    .csv(customersCsvLocation)
+    .as[Customer]
